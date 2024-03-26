f0fda163b6e824dd3148dbb05b5e2c034c5d4b42
V1nicius00 <vinicius.cordeiro23_@hotmail.com>
2024-03-25 23:16:48 -0300

    feat: creating Product model

---
+++ src/main/java/com/apiproducts/products/models/Product.java
@@ -0,0 +1,26 @@
+package com.apiproducts.products.models;
+
+import com.apiproducts.products.dtos.ProductDTO;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name="products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String name;
+    private Integer price_in_cents;
+
+    public Product(ProductDTO dto){
+        this.name = dto.name();
+        this.price_in_cents = dto.price_in_cents();
+    }
+}
